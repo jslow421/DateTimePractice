ef494d4090f8571dfa3ad39aeb93c81d54a7cf4f
jslowik <jslowik@10.30.20.184>
2015-10-26 16:06:32 -0500

    created utility class

--- src/jdk8/api/Demo2.java
+++ src/jdk8/api/Demo2.java
@@ -15,8 +15,6 @@ import java.util.Date;
 public class Demo2 {
     public static void main(String[] args) {
         // Get current date and time
-        Date dateNormal = new Date();//old format
-        System.out.println(dateNormal);
         LocalDateTime date = LocalDateTime.now();
         System.out.println("Current date/time with built-in LocalDateTime class format: " + date);
         
@@ -52,7 +50,7 @@ public class Demo2 {
         
         // Solution #1
         LocalDate today2 = LocalDate.now();
-        LocalDate lastBusDay = today2.with(TemporalAdjusters.lastInMonth(DayOfWeek.FRIDAY));
+        LocalDate lastBusDay = today2.with(TemporalAdjusters.lastInMonth(DayOfWeek.THURSDAY));
         System.out.println(lastBusDay.getDayOfMonth());
         
         // Find the difference between two dates, resulting in hours plus 
@@ -78,7 +76,7 @@ public class Demo2 {
         ZonedDateTime local = ZonedDateTime.now();
         local = local.withZoneSameInstant(ZoneId.of("Europe/Berlin"));
         
-        System.out.println("Date and time in Berlin, Germany " + local.format(fmt));
+        System.out.println(local.format(fmt));
         
         // Calculate a due date that is 15 days from now
         LocalDateTime now = LocalDateTime.now();
--- src/student/lab/Utilities/DateUtilities.java
+++ src/student/lab/Utilities/DateUtilities.java
@@ -8,7 +8,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import javax.swing.JOptionPane;
 
 /**
  *
